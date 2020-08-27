2a7512ae54ce23c15663539fa91a186cc5b22f83
Atai Ambus <atayambus@gmail.com>
2020-08-27 12:56:23 +0300

    clean(): remove annoying wpilib message

--- src/main/java/com/evergreen/everlib/shuffleboard/constants/commands/SetSwitch.java
+++ src/main/java/com/evergreen/everlib/shuffleboard/constants/commands/SetSwitch.java
@@ -10,13 +10,6 @@ import com.evergreen.everlib.shuffleboard.loggables.LoggableData;
 import com.evergreen.everlib.shuffleboard.loggables.LoggableString;
 import com.evergreen.everlib.utils.InstantEvergreenCommand;
 
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 /**A command that sets a {@link ConstantBoolean shuffleboard switch} an input value. <p>
  * This command has a shuffleboard switch (set at constucion), which, if diabled,
  * will stop this command from running.
