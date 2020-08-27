725340ab1edf2f5e586a29545bdedaae98a5e40e
Atai Ambus <atayambus@gmail.com>
2020-08-27 11:52:15 +0300

    feat(board/const/comm): add const path, target value  logs
    - To SetSwitch and SetConstant, added logs of the taregt Constant path and name, and the value to set the constant to, according to the current supplier output.  Initialized in constructor and added to getLoggableData.

--- src/main/java/com/evergreen/everlib/shuffleboard/constants/commands/SetConstant.java
+++ src/main/java/com/evergreen/everlib/shuffleboard/constants/commands/SetConstant.java
@@ -1,8 +1,13 @@
 package com.evergreen.everlib.shuffleboard.constants.commands;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 import com.evergreen.everlib.shuffleboard.constants.ConstantDouble;
+import com.evergreen.everlib.shuffleboard.loggables.LoggableData;
+import com.evergreen.everlib.shuffleboard.loggables.LoggableDouble;
+import com.evergreen.everlib.shuffleboard.loggables.LoggableString;
 import com.evergreen.everlib.utils.InstantEvergreenCommand;
 
 /**
@@ -11,6 +16,13 @@ import com.evergreen.everlib.utils.InstantEvergreenCommand;
  * @author Atai Ambus
  */
 public class SetConstant extends InstantEvergreenCommand {
+
+    /**The value the command will set the constant to */
+    Supplier<Double> m_valueToSet;
+
+    /**The path of the constant this command will set the value of */
+    String m_targetPath;
+    String m_targetName;
 
     /**
      * Constructs a new {@link SetConstant} command, which sets an input {@link ConstantDouble 
@@ -36,6 +48,20 @@ public class SetConstant extends InstantEvergreenCommand {
      */
     public SetConstant(String name, ConstantDouble constant, Supplier<Double> valueSupplier) {
         super(name, () -> constant.setValue(valueSupplier.get()));
+        m_valueToSet = valueSupplier;
+        m_targetPath = constant.getPath();
+        m_targetName = constant.getName();
+    }
+
+    @Override
+    public List<LoggableData> getLoggableData() {
+        ArrayList<LoggableData> res = new ArrayList<>(super.getLoggableData());
+        res.addAll(List.of(new LoggableData[] {
+            new LoggableDouble("Value to Set", m_valueToSet),
+            new LoggableString("Target Path", () -> m_targetPath),
+            new LoggableString("Target Name", () -> m_targetName)
+        }));
+        return res;
     }
 
 
