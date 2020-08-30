f66ffb1593601c12a5e489c1f525fe5a7ce381cd
peleg <peleghportal@gmail.com>
2020-08-30 15:09:04 +0300

    docs(board/consts/base): document constant implementation
    - whatever (Atai, 2020). Added javadoc to constants int and double

--- src/main/java/com/evergreen/everlib/shuffleboard/constants/ConstantInt.java
+++ src/main/java/com/evergreen/everlib/shuffleboard/constants/ConstantInt.java
@@ -5,36 +5,63 @@ import java.util.function.Supplier;
 import edu.wpi.first.wpilibj.Preferences;
 
 /**
- * LoggableNumber
+ * ConstantInt
+ * ConstantInt represent an int value in the shuffleboard - set with a
+ * value at RobotInit, and tuned by drivers and users in the shuffleboard.
  */
 public class ConstantInt extends Constant implements Supplier<Integer> {
     
+    /**
+     * The constant's default value.
+     */
     int m_defaultVal;
 
+    /***
+     * ConstantInt
+     * @param key the path, relative to the workin directory.
+     * @param initValue the constant's value
+     * Sets the constant in the given path with the value of initValue parameter.
+     */
     public ConstantInt(String key, int initValue) {
         super(key, initValue);
         m_defaultVal = initValue;
     }
     
+    /***
+     * addToDashboard
+     * Adds the constant to the Dashboard with the default value.
+     */
     @Override
     public void addToDashboard() {
         addToDashboard(m_defaultVal);
     }
 
+    /***
+     * addToDashboard
+     * @param value the value to be added to the Dashboard with the constant.
+     * Adds the constant to the Dashboard with the given value.
+     */
     @Override
     protected void addToDashboard(Object value) {
         Preferences.getInstance().putInt(getPath(), (int)value);
     }
 
-
+    /***
+     * getType
+     * Returns the type of the constant. It is Integer.
+     */
     @Override
     public String getType() {
         return "Integer";
     }
 
+    /***
+     * increaseAbsolute
+     * @param increaseBy increases the constant's value by this parameter's value.
+     * Increase the value of the constant by the value of the @param increaseBy parameter.
+     */
     public void increaseAbsolute(int increaseBy) {
-        m_defaultVal += increaseBy;
-        addToDashboard();
+        setValue(get() + increaseBy);
     }
 
     /**
@@ -43,15 +70,23 @@ public class ConstantInt extends Constant implements Supplier<Integer> {
      * @param maxValue - The 100% the percentage are a part of
      */
     public void increasePercentage(int percentage, int maxValue) {
-        m_defaultVal += maxValue * (percentage / 100);
-        addToDashboard();
+        setValue(get() + maxValue * percentage/100);
     } 
 
+    /***
+     * setValue
+     * @param newValue set the constant's valu to this parameter's value.
+     * Sets the constant's value to the value of the @param newValue parameter.
+     */
     public void setValue(int newValue) {
         m_defaultVal = newValue;
         reset();
     }
 
+    /***
+     * get
+     * Returns the constant's current value.
+     */
     @Override
     public Integer get() {
         return Preferences.getInstance().getInt(getPath(), m_defaultVal);
